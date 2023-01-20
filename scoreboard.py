72d6e1e4c96a01a941333c01aa6bfc77ad7b9d98
Camilo Santander <camilosantander@usantotomas.edu.co>
2023-01-19 22:15:01 -0500

    se añade el sistema de puntos

---
+++ scoreboard.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score=0
+        self.pu()
+        self.color("white")
+        self.hideturtle()
+        self.goto(0,265)
+        self.write(f"scoreboard = {self.score}",move= False, align="center", font =("Arial", 24, "normal"))
+        
+        
+    def prompt(self):
+        self.clear()
+        self.score += 1
+        self.write(f"scoreboard = {self.score}",move= False, align="center", font =("Arial", 24, "normal"))
+        
+        
